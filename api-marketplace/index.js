a7552a35743b7d9572878dc957264ca032527f4a
Donkami04cd <camilo.munerac@gmail.com>
2024-10-06 07:04:44 -0500

    Nuevo cors

--- api-marketplace/index.js
+++ api-marketplace/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
   'http://localhost:5173',
-  'https://magic-log.onrender.com/',
+  'https://magic-ws6m.onrender.com/',
   '*'
 ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
