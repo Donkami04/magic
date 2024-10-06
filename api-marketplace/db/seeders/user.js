b2f1c7288490d66109d1911953bafff0948d9c45
Donkami04cd <camilo.munerac@gmail.com>
2024-10-06 07:18:33 -0500

    Diferente usuario en el seeder

--- api-marketplace/db/seeders/user.js
+++ api-marketplace/db/seeders/user.js
@@ -4,8 +4,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('users', [
       {
-        name: 'Paulina',
-        email: 'paulina@mail.com',
+        name: 'Jorge',
+        email: 'jorge@mail.com',
         password: '$2b$10$Zc.ejEabujCNndC/w2SRX.Fh/bTpAvnPeRFdI9HL3fa588GO/p1oW',
         rol: 'vendedor',
       },
@@ -13,6 +13,6 @@ module.exports = {
   },
 
   down: async (queryInterface) => {
-    await queryInterface.bulkDelete('users', { email: 'paulina@mail.com' }, {});
+    await queryInterface.bulkDelete('users', { email: 'jorge@mail.com' }, {});
   },
 };
