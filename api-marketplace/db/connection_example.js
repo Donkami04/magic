bdf917960b82ce27a71e7ddae08284cf644f6f4b
Donkami04cd <camilo.munerac@gmail.com>
2024-10-06 08:59:08 -0500

    ip x localhost

--- api-marketplace/db/connection_example.js
+++ api-marketplace/db/connection_example.js
@@ -44,3 +44,21 @@ AeeGBFm3nLQqs7uyjv7eHXxLRNOnqWOknTp4wxwRp49YUN3eGg==
     },
   },
 };
+
+const { config } = require('../config/config');
+
+module.exports = {
+  development: {
+    url: config.dbUrl,
+    dialect: 'postgres',
+  },
+  production: {
+    url: config.dbUrl,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  }
+}
